dc727a2a46b64f856292bc670099a6a480fddbb3
Parkjunwu <phak9402@gmail.com>
2022-01-23 23:03:22 +0900

    end

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ApolloProvider, useReactiveVar } from '@apollo/client';
+import { HelmetProvider } from 'react-helmet-async';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { client, darkModeVar, isLoggedInVar } from './apollo';
+import Header from './components/Header';
+import Layout from './components/Layout';
+import routes from './routes';
+import Home from './screens/Home';
+import Login from './screens/Login';
+import NotFound from './screens/NotFound';
+import Profile from './screens/Profile';
+import SignUp from './screens/SignUp';
+import { darkTheme, GlobalStyle, lightTheme } from './styles';
 
 function App() {
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
+  const darkMode = useReactiveVar(darkModeVar);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <HelmetProvider>
+        <ThemeProvider theme={darkMode?lightTheme:darkTheme}>
+        <GlobalStyle/>
+          <Router>
+            <Switch>
+              <Route path={routes.home} exact >
+                {isLoggedIn?(
+                <Layout>
+                  <Home />
+                </Layout>
+                ):<Login />}
+              </Route>
+              {!isLoggedIn?<Route path={routes.signUp} exact >
+                <SignUp />
+              </Route> : null}
+              <Route path="/users/:userName" >
+                <Layout>
+                  <Profile />
+                </Layout>
+              </Route>
+              <Route>
+                <NotFound/>
+              </Route>
+              
+            </Switch>
+          </Router>
+        </ThemeProvider>
+      </HelmetProvider>
+    </ApolloProvider>
   );
 }
 
